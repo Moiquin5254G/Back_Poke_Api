10ea919673c7f27cdb1773adc909bcd755616e57
Moiquin5254G <JEstivenMoica_2@outlook.com>
2024-10-01 17:49:34 -0500

    Quité comentarios

--- src/controllers/getControllers/getControllersType/controllerGetTypesFromApi.js
+++ src/controllers/getControllers/getControllersType/controllerGetTypesFromApi.js
@@ -8,20 +8,16 @@ const { Type } = require("../../../db");
  */
 const controllerGetTypesFromApi = async () => {
   try {
-    // Verifica si ya hay tipos en la base de datos
     const existingTypes = await Type.findAll();
     console.log("Tipos existentes en la DB: ", existingTypes.map(t => t.name));
 
-    // Si ya existen tipos en la base de datos, devuélvelos
     if (existingTypes.length > 0) {
       return existingTypes.map((type) => type.name);
     }
 
-    // Si no existen, trae los tipos de la API
     const response = await axios.get("https://pokeapi.co/api/v2/type");
     console.log("Tipos obtenidos de la API: ", response.data.results);
 
-    // Mapea y guarda los tipos en la base de datos
     const typesFromApi = response.data.results.map((type) => ({
       name: type.name,
       url: type.url,
@@ -29,7 +25,6 @@ const controllerGetTypesFromApi = async () => {
 
     console.log("Tipos mapeados desde la API: ", typesFromApi);
 
-    // Guarda los tipos en la base de datos
     await Promise.all(
       typesFromApi.map((type) =>
         Type.findOrCreate({
@@ -39,7 +34,6 @@ const controllerGetTypesFromApi = async () => {
       )
     );
 
-    // Devuelve solo los nombres de los tipos
     const types = typesFromApi.map((type) => type.name);
     console.log("Tipos que se devolverán: ", types);
 
